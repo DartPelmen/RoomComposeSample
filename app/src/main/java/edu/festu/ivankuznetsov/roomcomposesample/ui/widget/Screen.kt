70497cb9f214c0b9e2eaac318a6f2e458351ae7c
DartPelmen <kyzaiv1@yandex.ru>
2024-02-24 15:18:30 +1000

    Adding in Screen.kt

--- app/src/main/java/edu/festu/ivankuznetsov/roomcomposesample/ui/widget/Screen.kt
+++ app/src/main/java/edu/festu/ivankuznetsov/roomcomposesample/ui/widget/Screen.kt
@@ -1,10 +1,6 @@
 package edu.festu.ivankuznetsov.roomcomposesample.ui.widget
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.magnifier
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,20 +8,38 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
+import edu.festu.ivankuznetsov.roomcomposesample.database.entity.Event
+import edu.festu.ivankuznetsov.roomcomposesample.database.entity.embedded.EventInfo
 import edu.festu.ivankuznetsov.roomcomposesample.ui.model.EventModel
 import edu.festu.ivankuznetsov.roomcomposesample.ui.viewmodel.AppViewModelProvider
 import edu.festu.ivankuznetsov.roomcomposesample.ui.viewmodel.EventListViewModel
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 
 
 @Composable
-fun Screen(modifier: Modifier = Modifier,
-           eventViewModel: EventListViewModel = viewModel(factory = AppViewModelProvider.Factory)){
+fun Screen(
+    modifier: Modifier = Modifier,
+    eventViewModel: EventListViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
     val eventListState by eventViewModel.retrieveEvents().collectAsState(listOf())
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Button(onClick = { Log.d("SCREEN", "CLICKED") }) {
+        Button(onClick = {
+            eventViewModel.viewModelScope.launch {
+                eventViewModel.add(
+                    Event(
+                        eventInfo = EventInfo(
+                            eventName = "My Name",
+                            eventDescription = "blank",
+                            startDate = LocalDateTime.now(),
+                            endDate = LocalDateTime.now()
+                        )
+                    )
+                )
+            }
+        }) {
             Text(text = "NEW EVENT")
         }
         EventList(modifier = modifier, eventListState.map { EventModel(it) })
