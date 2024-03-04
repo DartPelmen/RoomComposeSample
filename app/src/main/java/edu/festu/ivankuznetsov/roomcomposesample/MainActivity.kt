aaf23c720cb570bd6fdc691237b937819ed38b8b
DartPelmen <kyzaiv1@yandex.ru>
2024-03-05 08:22:01 +1000

    a little koin added

--- app/src/main/java/edu/festu/ivankuznetsov/roomcomposesample/MainActivity.kt
+++ app/src/main/java/edu/festu/ivankuznetsov/roomcomposesample/MainActivity.kt
@@ -1,35 +1,14 @@
 package edu.festu.ivankuznetsov.roomcomposesample
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import edu.festu.ivankuznetsov.roomcomposesample.ui.theme.RoomComposeSampleTheme
-import edu.festu.ivankuznetsov.roomcomposesample.ui.viewmodel.EventListViewModel
-import edu.festu.ivankuznetsov.roomcomposesample.ui.widget.PreviewEventList
 import edu.festu.ivankuznetsov.roomcomposesample.ui.widget.Screen
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
